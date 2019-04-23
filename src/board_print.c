d20cd2dfece57b96a6e8d438d5b760422da724f6
Лозовой Владислав Александрович <iv823s11@awp11.csc.local>
2019-04-23 12:13:37 +0700

    Try to fix codestyle #3

--- src/board_print.c
+++ src/board_print.c
@@ -5,19 +5,19 @@ extern char board[8][8];
 
 void printboard()
 {
-	int i, j;
+    int i, j;
 
-	for (i = 0; i < 8; i++) {
-		printf("%d| ", i + 1);
-		for (j = 0; j < 8; j++) {
-			printf("%c ", board[i][j]);
-		}
-		printf("\n");
-	}
+    for (i = 0; i < 8; i++) {
+        printf("%d| ", i + 1);
+        for (j = 0; j < 8; j++) {
+            printf("%c ", board[i][j]);
+        }
+        printf("\n");
+    }
 
-	printf("   ");
+    printf("   ");
 
-	for (i = 0; i < 8; i++)
-	printf("%c ", i + 65);
-	printf("\n");
+    for (i = 0; i < 8; i++)
+    printf("%c ", i + 65);
+    printf("\n");
 }
